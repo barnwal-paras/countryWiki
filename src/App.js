814abd24631d9cae5b40c4115dc78c955fbf148a
parasbarnwal06 <paras.kumar@imanage.com>
2019-12-27 19:25:18 +0530

    added projectt files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Search from "./Component/Search";
+import Country from "./Component/Country";
+import Countrylist from "./Component/CountryList";
 
 function App() {
+  const [data, setdata] = useState([]);
+  const [text, updatetext] = useState("");
+  const [cnt, setcnt] = useState(false);
+
+  const getData = () => {
+    axios.get("https://restcountries.eu/rest/v2/all").then(response => {
+      console.log(response.data);
+      setdata(response.data);
+    });
+  };
+
+  const search = value => {
+    setcnt(false);
+    updatetext(value);
+  };
+
+  const showCountry = () => {
+    if (cnt != false) {
+      return <Country data={data.filter(country => country.name == cnt)[0]} />;
+    } else {
+      return <div></div>;
+    }
+  };
+
+  const selectCountry = value => {
+    setcnt(value);
+  };
+
+  const filter = () => {
+    if (text == "") {
+      return <div>No value provided for search</div>;
+    }
+    const filtered_data = data.filter(country =>
+      country.name.toLowerCase().includes(text.toLowerCase())
+    );
+    if (filtered_data.length == 0) {
+      return <div>No country with such name.</div>;
+    } else if ((filtered_data.length == 1) & !cnt) {
+      selectCountry(filtered_data[0].name);
+      return <div></div>;
+    } else if (!cnt) {
+      // console.log(<Countrylist name="country.name" show="selectCountry" />);
+      // console.log(filtered_data.map(country => country.name));
+      return filtered_data.map(country => (
+        <Countrylist
+          name={country.name}
+          show={() => selectCountry(country.name)}
+        />
+      ));
+    }
+    return <div></div>;
+  };
+
+  useEffect(getData, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search search={search} />
+      {filter()}
+      <div>{showCountry()}</div>
     </div>
   );
 }
