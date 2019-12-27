c6550e1748c52795cc3808add5c62d7a1fa8483d
parasbarnwal06 <paras.kumar@imanage.com>
2019-12-27 19:39:40 +0530

    Updates

---
+++ precache-manifest.fc0f48882be7371cdaf25a27ec24c120.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc6536461169d9fc431fdf3502a28be",
+    "url": "/countryWiki/index.html"
+  },
+  {
+    "revision": "bdda2046debab9f71545",
+    "url": "/countryWiki/static/js/2.ef40cde0.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/countryWiki/static/js/2.ef40cde0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab1bdcc1f2903bf5b7e0",
+    "url": "/countryWiki/static/js/main.649d4185.chunk.js"
+  },
+  {
+    "revision": "f65c117d1a0a76410c39",
+    "url": "/countryWiki/static/js/runtime-main.b0da5d7e.js"
+  }
+]);
